28d97b097f8ad9190f615b7320ccab8376f2ecc7
Tsuyoshi Fukuzawa <tsuyoshi.fukuzawa@gmail.com>
2020-01-07 11:11:31 +0900

    最小構成

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,12 +2,11 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path')
 
 const htmlWebpackPlugin = new HtmlWebPackPlugin({
-  template: "./src/client/index.html",
+  template: "./src/index.html",
   filename: "./index.html"
 });
-
-module.exports = {
-  entry: "./src/client/index.js",
+ module.exports = {
+  entry: "./src/index.js",
   output: {
     path: path.resolve('dist'),
     filename: '[name].js'
@@ -18,10 +17,14 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: "babel-loader"
         }
       }
+      // {
+      //   test: /\.css$/,
+      //   use: ["style-loader", "css-loader"]
+      // }
     ]
   },
   plugins: [htmlWebpackPlugin]
-};
+}; 
